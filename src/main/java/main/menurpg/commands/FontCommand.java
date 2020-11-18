cd76e513b6452ee2f6c47e283d832803349ac41b
rain_chat <super.pasha4444@yandex.ru>
2020-11-18 13:26:24 +1000

    1.2.01-beta (small fix)

--- src/main/java/main/menurpg/commands/FontCommand.java
+++ src/main/java/main/menurpg/commands/FontCommand.java
@@ -16,59 +16,25 @@ import org.bukkit.event.player.PlayerSwapHandItemsEvent;
 public class FontCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)){
+            return true;
+        }
+
 
         Player player = (Player) sender;
+        if (args.length > 1){
+            player.sendMessage("/rpgmenu");
+            return true;
+        }
 
-
-
-        //FileManager fileManager = new FileManager(main.getPlugin(main.class));
-        //fileManager.reloadConfig("config.yml");
-        //player.sendMessage(ChatColor.GREEN+"Конфиг перезагружен");
-        char y;
-        BaseComponent textcopy = new TextComponent("");
-        if (args[1].equalsIgnoreCase("1")){
-            for (char s : args[2].toCharArray()) {
-                FrontLevel1 dFI = FrontLevel1.getDefaultFontInfo(s);
-                y = dFI.getfont();
-                textcopy.addExtra(y + "");
-            }
+        if (player.hasPermission("rpgmenu.admin")){
+            FileManager fileManager = new FileManager(main.getPlugin(main.class));
+            fileManager.reloadConfig("config.yml");
+            player.sendMessage(ChatColor.GREEN+"Конфиг перезагружен...");
         }
-        if (args[1].equalsIgnoreCase("2")){
-            for (char s : args[2].toCharArray()) {
-                FrontLevel2 dFI = FrontLevel2.getDefaultFontInfo(s);
-                y = dFI.getfont();
-                textcopy.addExtra(y + "");
-            }
+        else {
+            player.sendMessage("У вас нет прав на эту команду!");
         }
-        if (args[1].equalsIgnoreCase("3")){
-            for (char s : args[2].toCharArray()) {
-                FrontLevel3 dFI = FrontLevel3.getDefaultFontInfo(s);
-                y = dFI.getfont();
-                textcopy.addExtra(y + "");
-            }
-        }
-        if (args[1].equalsIgnoreCase("4")){
-            for (char s : args[2].toCharArray()) {
-                FrontLevel4 dFI = FrontLevel4.getDefaultFontInfo(s);
-                y = dFI.getfont();
-                textcopy.addExtra(y + "");
-            }
-        }
-        if (args[1].equalsIgnoreCase("5")){
-            for (char s : args[2].toCharArray()) {
-                FrontLevel5 dFI = FrontLevel5.getDefaultFontInfo(s);
-                y = dFI.getfont();
-                textcopy.addExtra(y + "");
-            }
-        }
-        if (args[1].equalsIgnoreCase("6")){
-            for (char s : args[2].toCharArray()) {
-                FrontLevel6 dFI = FrontLevel6.getDefaultFontInfo(s);
-                y = dFI.getfont();
-                textcopy.addExtra(y + "");
-            }
-        }
-        player.spigot().sendMessage(ChatMessageType.ACTION_BAR, textcopy);
 
 
         return true;
